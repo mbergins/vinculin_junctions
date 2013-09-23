2abb42b43afa984d3e97d6efe6e0e458cb05ad53
Matthew Berginski <matthew.berginski@gmail.com>
2013-09-23 10:17:47 -0400

    changes associated with vinculin images

--- src/find_cell_mask.m
+++ src/find_cell_mask.m
@@ -14,47 +14,43 @@ start_boundary = tic;
 %%Main Program
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-tdTom_files = dir(fullfile(exp_dir,'tdTom'));
-tdTom_files = tdTom_files(3:end);
+vinc_files = dir(fullfile(exp_dir,'Vinculin'));
+vinc_files = vinc_files(3:end);
 
 mkdir(fullfile(exp_dir,'cell_region_highlight'));
 mkdir(fullfile(exp_dir,'cell_region_binary'));
 
-tdTom_norm = cell(length(tdTom_files),1);
-tdTom_med_filt = cell(length(tdTom_files),1);
-threshold_set = zeros(length(tdTom_files),1);
-for i = 1:length(tdTom_files)
-    tdTom = double(imread(fullfile(exp_dir,'tdTom',tdTom_files(i).name)));
-    tdTom_norm{i} = (tdTom - min(tdTom(:)))/range(tdTom(:));
+vinc_norm = cell(length(vinc_files),1);
+vinc_med_filt = cell(length(vinc_files),1);
+threshold_set = zeros(length(vinc_files),1);
+for i = 1:length(vinc_files)
+    vinc = double(imread(fullfile(exp_dir,'Vinculin',vinc_files(i).name)));
+    vinc_norm{i} = (vinc - min(vinc(:)))/range(vinc(:));
 
-    tdTom_med_filt{i} = medfilt2(tdTom,[11,11],'symmetric');
+    vinc_med_filt{i} = medfilt2(vinc,[11,11],'symmetric');
     
-    threshold_set(i) = determine_threshold(tdTom_med_filt{i}(:));
+    threshold_set(i) = determine_threshold(vinc_med_filt{i}(:));
     
     if (mod(i,10) == 0)
         disp(['Done finding threshold in image number: ',num2str(i)]);
     end
 end
 
-%only use the first 20 images to select median because the wound won't have
-%closed by then, so we can expect the dual peak detection to work
-final_threshold = median(threshold_set(1:20));
-
-for i = 1:length(tdTom_files)
-    background_region = tdTom_med_filt{i} < final_threshold;
+for i = 1:length(vinc_files)
+    background_region = vinc_med_filt{i} < threshold_set(i);
     background_region = imfill(background_region,'holes');
 
     cell_region = not(background_region);
     output_file = fullfile(exp_dir,'cell_region_binary',sprintf('%03d.png',i));
     imwrite(cell_region,output_file);
 
-    cell_highlight = create_highlighted_image(tdTom_norm{i},cell_region,'mix_percent',0.25);
+    cell_highlight = create_highlighted_image(vinc_norm{i},cell_region,'mix_percent',0.25);
     output_file = fullfile(exp_dir,'cell_region_highlight',sprintf('%03d.png',i));
     imwrite(cell_highlight,output_file);
 end
 
 fprintf('Done finding the cell regions, total time: %d, each image took: %d\n', ...
-    toc(start_boundary),toc(start_boundary)/length(tdTom_files))
+    toc(start_boundary),toc(start_boundary)/length(vinc_files))
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%Functions
